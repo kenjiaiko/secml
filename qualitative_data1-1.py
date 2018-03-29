bdaeebfc6456e59f64851b6af0157f3ee64032fe
Aiko Kenji <JP20545@w022501604040m.local>
2018-03-29 20:19:21 +0900

    add qualitative

---
+++ qualitative_data1-1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+result = {}
+
+dataset = [
+	"ECAEA", "ECAAD", "ECACA", "ECABB", "ECACA", # ECA**
+	"FBABF", "ABABC", "DBABA",                   # *BAB*
+	"ABCDE",
+	"DDFEA",
+	"BCDAA",
+	"FFFED",
+	"AAAAA",
+	"EEEEE",
+	"BBDBB"
+]
+
+N = len(dataset)
+for i in range(N):
+	for j in range(i+1, N):
+		_cmp = ""
+		for k in range(len(dataset[0])):
+			if dataset[i][k] == dataset[j][k]:
+				_cmp += dataset[i][k]
+			else:
+				_cmp += "*"
+		if len(list(set(_cmp))) == 1 and _cmp[0] == "*":
+			continue
+		result.setdefault(_cmp, 0)
+		result[_cmp] += 1
+
+print "SCORE: "
+for k, v in sorted(result.items(), key=lambda x:x[1], reverse=True):
+	print k, v
+print
+
+total = {}
+print "DATASET: "
+for d in dataset:
+	_total = 0
+	for k in result.keys():
+		_all = 0
+		_cnt = 0
+		for i in range(len(dataset[0])):
+			if k[i] == "*":
+				continue
+			_all += 1
+			if d[i] == k[i]:
+				_cnt += 1
+		if _all == _cnt:
+			_total += result[k] * (6 ** (_all - 1))
+	total.setdefault(d, 0)
+	total[d] = _total
+
+for k, v in sorted(total.items(), key=lambda x:x[1], reverse=True):
+	print k, v
+
